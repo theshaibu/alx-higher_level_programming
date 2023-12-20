6133ad00c0ab2da08831db63037152091c8df117
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-12-20 05:30:47 -0800

    class Node that defines a node of a singly linked

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if not self.head or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node and value > current.next_node.data:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        result = ""
+        current = self.head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result
+
+
+# Example usage:
+linked_list = SinglyLinkedList()
+linked_list.sorted_insert(5)
+linked_list.sorted_insert(3)
+linked_list.sorted_insert(7)
+print(linked_list)
+
