209dc4560b192bee1c23ab2297b9abb5a3e6d43d
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-04 14:14:00 -0800

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,40 +3,52 @@
 Defines a Rectangle class.
 """
 
+
 class Rectangle:
+    """Rectangle class defined by width and height."""
+
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        """Initializes a Rectangle instance.
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self._width
+        """Retrieves the width of a Rectangle instance."""
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of a Rectangle instance
+
+        Args:
+            value: value of the width, must be a positive integer
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        """Retrieves the height of a Rectangle instance."""
+        return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of a Rectangle instance
+
+        Args:
+            value: value of the height, must be a positive integer
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self._height = value
-
-    def area(self):
-        return self._width * self._height
-
-    def perimeter(self):
-        return 2 * (self._width + self._height) if self._width > 0 and self._height > 0 else 0
-
+        self.__height = value
