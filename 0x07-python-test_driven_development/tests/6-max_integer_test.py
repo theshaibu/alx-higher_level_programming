59cfcd84af51834f7fd260e2543a252d2762ad02
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-08 15:35:48 -0800

    Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxIntegerRegular(unittest.TestCase):
+    # ... Test cases for regular scenarios ...
+
+class TestMaxIntegerEdgeCases(unittest.TestCase):
+    # ... Test cases for edge cases ...
+
+class TestMaxIntegerExceptions(unittest.TestCase):
+    # ... Test cases for exception scenarios ...
+
+if __name__ == '__main__':
+    unittest.main()
+
