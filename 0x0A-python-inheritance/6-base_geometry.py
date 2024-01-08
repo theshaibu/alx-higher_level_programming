9263bfc2d1f015a85fd785847d90a18faa6a2710
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-08 14:25:15 -0800

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+"""
+
+class BaseGeometry:
+    """
+    Raises an Exception with the message area() is not implemented
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
