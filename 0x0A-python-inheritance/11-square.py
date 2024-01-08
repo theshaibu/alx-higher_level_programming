82a1e4845143247fdb6a139fd7ab82b3738f0df8
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-08 14:39:58 -0800

    corrections

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,32 +3,33 @@
 This is a class Square that inherits from Rectangle class as the base class
 """
 
-from typing import Union
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """This is a square class
+    """This is a rectangle class
     It has the following private attribute:
         size = size
     It inherits the integer validator from the Rectangle class
     """
 
-    def __init__(self, size: Union[int, float]):
+    def __init__(self, size):
         """Validates the following attributes:
             - size
         """
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
+    
     def __str__(self):
         """Returns a format string of the attribute size"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
 
+        return("[Square] {}/{}".format(self.__size, self.__size))
+    
     def area(self):
         """Returns the area of the Square instance"""
-        return self.__size ** 2
 
+        return self.__size **2
