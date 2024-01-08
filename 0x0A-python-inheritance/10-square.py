c18f55f48ba5b1ab6074c7066555796e17c6513a
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-08 14:36:07 -0800

    class Square that inherits from Rectangle (9-rectangle.py)

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module: 10-square
+This is a class Square that inherits from Rectangle class as the base class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This is a square class
+    It has the following private attribute:
+        size = size
+    It inherits the integer validator from the Rectangle class
+    """
+
+    def __init__(self, size):
+        """Validates the following attributes:
+            - size
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns a super() string of the attribute size"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Returns the area of the attribute size"""
+        return self.__size ** 2
+
