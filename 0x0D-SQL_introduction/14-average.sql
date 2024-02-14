e90e0da8eb38d0f09f5d00ca03b4fc8038e46462
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-02-14 15:45:44 -0800

    corrections

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
 -- Compute the average score of all records in the table 'second_table' of the database 'hbtn_0c_0'
-SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
-
+SELECT AVG(`score`) 'average' FROM second_table;
