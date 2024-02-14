ab5c1d23a1533f31f0be45797b60cc4e9fa4f059
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-02-14 15:32:46 -0800

    corrections

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
--- List all rows of the table 'first_table' from the database 'hbtn_0c_0'
-SELECT * FROM hbtn_0c_0.first_table;
-
+-- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- it displays all the table rows
+SELECT * FROM first_table;
