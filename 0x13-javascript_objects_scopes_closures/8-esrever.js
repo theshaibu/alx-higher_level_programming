dc68b8ce97feb210b7edbe86796969af9f81f07c
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-03-12 15:31:47 -0700

    function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a function that returns the reversed version of a list without using reverse function
+
+exports.esrever = function(list) {
+    const reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+        reversedList.push(list[i]);
+    }
+    return reversedList;
+};
+
