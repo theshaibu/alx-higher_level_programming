a11d987c612238ebbe5377d953c881099e027391
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-03-12 15:48:38 -0700

    script that concats 2 files.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+try {
+    const data1 = fs.readFileSync(sourceFile1, 'utf8');
+    const data2 = fs.readFileSync(sourceFile2, 'utf8');
+    const combinedData = data1 + data2;
+    fs.writeFileSync(destinationFile, combinedData);
+} catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+}
+
